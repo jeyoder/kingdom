cbbf5f221d61ba89bafa3356f1dd1d220aac4b11
James Yoder <james@jyoder.com>
2013-09-12 10:33:57 -0500

    Hello world

---
+++ Kingdom/main.cpp
@@ -0,0 +1,15 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Sep 12, 2013
+ *      Author: crhscs264
+ */
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+	cout << "hello world";
+	return 0;
+}
+
