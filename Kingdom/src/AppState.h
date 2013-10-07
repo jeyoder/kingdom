e318e452343a4f6eed5ae85bbc8b8c31b6fdd83b
James Yoder <james@jyoder.com>
2013-10-07 10:22:42 -0500

    Game and using InGameState

--- Kingdom/src/AppState.h
+++ Kingdom/src/AppState.h
@@ -7,7 +7,7 @@
 
 #ifndef APPSTATE_H_
 #define APPSTATE_H_
-
+#include "SDL.h"
 namespace kingdom {
 /*Keeps track of what state the program is in
  * ex. InGame, InLobby, InSettings
@@ -16,7 +16,7 @@ class AppState {
 public:
 	AppState();
 	virtual ~AppState();
-	virtual bool render() = 0; //Abstract method Renders the game state
+	virtual bool render(SDL_Renderer* renderer, SDL_Window* window, double delta, const Uint8* keystates) = 0; //Abstract method Renders the game state
 };
 
 } /* namespace std */
--- Kingdom/src/main.cpp
+++ Kingdom/src/main.cpp
@@ -14,6 +14,7 @@
 #include "InGameState.h"
 #include "FileMapGenerator.h"
 #include "TileMap.h"
+#include "Game.h"
 using namespace std;
 using namespace kingdom;
 SDL_Window* window;
@@ -28,73 +29,8 @@ int main(int argc, char* argv[]) {
 			SDL_RENDERER_ACCELERATED);
 	cout << "SDL initialized" << endl;
 
-	ResourceLoader* loader = ResourceLoader::getInstance(renderer);
-	loader->loadTexture("assets/thor.png");
-	SDL_Texture *texture = loader->loadTexture("assets/thor.png");
-
-	loader->loadTexture("assets/KingGreen.png");
-	loader->loadTexture("assets/KingRed.png");
-
-	SDL_Texture *tileset = loader->loadTexture("assets/tileset-1.png");
-	ifstream stream;
-	stream.open("assets/tilemap.txt");
-	if (!stream.is_open()) {
-		cout << "map file open failed!" << endl;
-		return 1;
-	}
-	cout << "Tilemap opened successfully" << endl;
-	FileMapGenerator generator(&stream);
-	TileMap map = TileMap(&generator, tileset);
-	cout << "Map loaded" << endl;
-	cout << "Map width: " << map.getW() << endl;
-	cout << "Map height: " << map.getH() << endl;
-	cout << map.mapData.size() << " tiles" << endl;
-	cout.flush();
-
-	//Initialize App State
-	InGameState currentAppState = InGameState();
-	//Game Loop
-	double tileX = 40;
-	double tileY = 40;
-	double moveSpeed = .1;
-	clock_t startTime;
-	int frameCount = 0;
-	startTime = clock();
-	while (true) {
-		 SDL_Event e;
-		        while (SDL_PollEvent(&e)) {
-		            if (e.type == SDL_QUIT) {
-		                return 0;
-		            }
-		        }
-
-		        const Uint8 *keystates = SDL_GetKeyboardState(NULL);
-		        if(keystates[SDL_SCANCODE_W]) {
-		        	tileY -= moveSpeed;
-		        }
-		        if(keystates[SDL_SCANCODE_A]) {
-					tileX -= moveSpeed;
-				}
-		        if(keystates[SDL_SCANCODE_S]) {
-					tileY += moveSpeed;
-				}
-		        if(keystates[SDL_SCANCODE_D]) {
-					tileX += moveSpeed;
-				}
-		        if(keystates[SDL_SCANCODE_ESCAPE]) {
-		        	return 0;
-		        }
-		SDL_RenderClear(renderer);
-		map.draw(renderer, window, tileX, tileY);
-		SDL_RenderPresent(renderer);
-		frameCount++;
-		if(((double)(clock() - startTime)) / CLOCKS_PER_SEC > 1.0) {
-			cout << frameCount << " FPS" << endl;
-			frameCount = 0;
-			startTime = clock();
-		}
-	}
-
+	Game game(renderer, window);
+	game.run();
 	SDL_DestroyRenderer(renderer);
 	SDL_DestroyWindow(window);
 
